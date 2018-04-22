5544d2e914a630816cccf969b6c876751239bfed
Brandon Scott <brandongregoryscott@gmail.com>
2018-04-22 18:50:09 -0400

    Testing environment variables

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import io
 import random
 import logging
 import sys
+import os
+import ast
 
 from twitterbot import TwitterBot, ignore
 
@@ -19,53 +21,46 @@ class Bot(TwitterBot):
         ############################
         # REQUIRED: LOGIN DETAILS! #
         ############################
-        self.config['api_key'] = ''
-        self.config['api_secret'] = ''
-        self.config['access_key'] = ''
-        self.config['access_secret'] = ''
+        self.config['api_key'] = os.environ['API_KEY']
+        self.config['api_secret'] = os.environ['API_SECRET']
+        self.config['access_key'] = os.environ['ACCESS_KEY']
+        self.config['access_secret'] = os.environ['ACCESS_SECRET']
 
         ######################################
         # SEMI-OPTIONAL: OTHER CONFIG STUFF! #
         ######################################
 
         # how often to tweet, in seconds
-        self.config['tweet_interval'] = 60 * 60  # default: 30 minutes
+        self.config['tweet_interval'] = os.environ['TWEET_INTERVAL']  # default: 30 minutes
 
         # use this to define a (min, max) random range of how often to tweet
         # e.g., self.config['tweet_interval_range'] = (5*60, 10*60) # tweets every 5-10 minutes
-        self.config['tweet_interval_range'] = (45 * 60, 75 * 60)
+        # self.config['tweet_interval_range'] = (45 * 60, 75 * 60)
 
         # only reply to tweets that specifically mention the bot
-        self.config['reply_direct_mention_only'] = False
+        self.config['reply_direct_mention_only'] = ast.literal_eval(os.environ['REPLY_DIRECT_MENTION_ONLY'])
 
         # only include bot followers (and original tweeter) in @-replies
-        self.config['reply_followers_only'] = True
+        self.config['reply_followers_only'] = ast.literal_eval(os.environ['REPLY_FOLLOWERS_ONLY'])
 
         # fav any tweets that mention this bot?
-        self.config['autofav_mentions'] = False
+        self.config['autofav_mentions'] = ast.literal_eval(os.environ['AUTOFAV_MENTIONS'])
 
         # fav any tweets containing these keywords?
-        self.config['autofav_keywords'] = []
+        self.config['autofav_keywords'] = ast.literal_eval(os.environ['AUTOFAV_KEYWORDS'])
 
         # follow back all followers?
-        self.config['autofollow'] = False
+        self.config['autofollow'] = ast.literal_eval(os.environ['AUTOFOLLOW'])
+
 
         ###########################################
         # CUSTOM: your bot's own config variables! #
         ###########################################
 
-        self.config['bots'] = ['imgshflbot', 'artyabstract', 'artycrush', 'artycurve', 'abstractedbot']
-        self.config['tags'] = ['Here, take this image - it\'s your problem now!',
-                               'I did the best I could. Can you take it from here?',
-                               'Here you go.']
-        self.config['responses'] = ['Here, take this image - you wanted it!',
-                                    'I did the best I could.',
-                                    'Is this what you wanted?',
-                                    'This image is pretty average now.',
-                                    'Average, at best.',
-                                    'On average, this is what your image looks like.',
-                                    'Everything feels a little blurry now.']
-        
+        self.config['bots'] = ast.literal_eval(os.environ['BOTS'])
+        self.config['tags'] = ast.literal_eval(os.environ['TAGS'])
+        self.config['responses'] = ast.literal_eval(os.environ['RESPONSES'])
+
     def on_scheduled_tweet(self):
         return
         mention_user = random.choice(self.config['bots'])
